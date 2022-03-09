c582f1b8cceb462631c41adacad7904d1ecd9956
leobatouxas <leo.batouxas@gmail.com>
2022-03-09 11:09:16 +0100

    Ajout page Ajouter

--- app/src/main/java/fr/leobatouxas/gestionculture/ui/ajouter/AjouterFragment.java
+++ app/src/main/java/fr/leobatouxas/gestionculture/ui/ajouter/AjouterFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import fr.leobatouxas.gestionculture.databinding.FragmentAjouterBinding;
 
@@ -20,8 +18,7 @@ public class AjouterFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAjouterBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        return binding.getRoot();
     }
 
     @Override
