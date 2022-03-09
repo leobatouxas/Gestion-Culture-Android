b7eb8a798e4b58aec3d5d314cd6a6458e45f52b0
leobatouxas <leo.batouxas@gmail.com>
2022-03-09 14:58:51 +0100

    Ajout page Ajout exploitant

--- app/src/main/java/fr/leobatouxas/gestionculture/MainActivity.java
+++ app/src/main/java/fr/leobatouxas/gestionculture/MainActivity.java
@@ -1,6 +1,9 @@
 package fr.leobatouxas.gestionculture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
--- app/src/main/java/fr/leobatouxas/gestionculture/ui/ajouter/AjouterFragment.java
+++ app/src/main/java/fr/leobatouxas/gestionculture/ui/ajouter/AjouterFragment.java
@@ -1,13 +1,20 @@
 package fr.leobatouxas.gestionculture.ui.ajouter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import fr.leobatouxas.gestionculture.AjoutExploitant;
+import fr.leobatouxas.gestionculture.MainActivity;
+import fr.leobatouxas.gestionculture.R;
 import fr.leobatouxas.gestionculture.databinding.FragmentAjouterBinding;
 
 
@@ -17,13 +24,28 @@ public class AjouterFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentAjouterBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View rootView = inflater.inflate(R.layout.fragment_ajouter, container, false);
+
+
+        Button btn = rootView.findViewById(R.id.btnExpNonExistant);
+        btn.setOnClickListener(btnAjouterExploitant);
+
+        return rootView;
+
     }
+
+    private final View.OnClickListener btnAjouterExploitant = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), AjoutExploitant.class);
+             ((MainActivity) getActivity()).startActivity(intent);
+        }
+    };
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
