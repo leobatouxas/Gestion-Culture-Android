9d3885907fc44c44ea61efae9c1e3bfc2b3eb71d
leobatouxas <leo.batouxas@gmail.com>
2022-05-06 12:18:06 +0200

    verification existence en base de données

--- API/insert.php
+++ API/insert.php
@@ -1,44 +1,51 @@
 <?php
 include "function.php";
 
-if(isset($_REQUEST["operation"])) {
-    if($_REQUEST["operation"] == "enreg") {
-        try{
+if (isset($_REQUEST["operation"])) {
+    if ($_REQUEST["operation"] == "enreg") {
+        try {
             $cnx = connexionPDO();
             $donneesjson = $_REQUEST["donnees"];
             $donnees = json_decode($donneesjson);
             $codeExploitant = $donnees[0];
-            foreach($donnees as $cahierCulture) {
+            foreach ($donnees as $cahierCulture) {
                 $idCahierCulture = $cahierCulture->idCahierCulture;
                 $annee = $cahierCulture->annee;
                 $codeExploitation = $cahierCulture->codeExploitation;
-                $req = "INSERT INTO cahierculture (annee, codeExploitation) VALUES (:annee, :codeExploitation)";
-                $stmt = $cnx->prepare($req);
-                $stmt->bindParam(":annee", $annee);
+                $reqselect = "SELECT codeExploitation FROM exploitation WHERE codeExploitation = :codeExploitation";
+                $stmt = $cnx->prepare($reqselect);
                 $stmt->bindParam(":codeExploitation", $codeExploitation);
                 $stmt->execute();
-
-                $req = "SELECT idCahierCulture FROM cahierculture WHERE annee = :annee AND codeExploitation = :codeExploitation";
-                $stmt = $cnx->prepare($req);
-                $stmt->bindParam(":annee", $annee);
-                $stmt->bindParam(":codeExploitation", $codeExploitation);
-                $stmt->execute();
-                $idCahierCulture = $stmt->fetch()["idCahierCulture"];
-
-                foreach($cahierCulture->parcelles as $parcelle) {
-                    $req = "INSERT INTO parcelle (codeEspece, idCahierCulture, surface, rendementPrevu, rendementRealise) VALUES (:codeEspece, :idCahierCulture, :surface, :rendementPrevu, :rendementRealise)";
+                $result = $stmt->fetchAll();
+                if ($result != null) {
+                    $req = "INSERT INTO cahierculture (annee, codeExploitation) VALUES (:annee, :codeExploitation)";
                     $stmt = $cnx->prepare($req);
-                    $stmt->bindParam(":codeEspece", $parcelle->codeEspece);
-                    $stmt->bindParam(":idCahierCulture", $idCahierCulture);
-                    $stmt->bindParam(":surface", $parcelle->surface);
-                    $stmt->bindParam(":rendementPrevu", $parcelle->rendementPrevu);
-                    $stmt->bindParam(":rendementRealise", $parcelle->rendementRealise);
+                    $stmt->bindParam(":annee", $annee);
+                    $stmt->bindParam(":codeExploitation", $codeExploitation);
                     $stmt->execute();
+
+                    $req = "SELECT idCahierCulture FROM cahierculture WHERE annee = :annee AND codeExploitation = :codeExploitation";
+                    $stmt = $cnx->prepare($req);
+                    $stmt->bindParam(":annee", $annee);
+                    $stmt->bindParam(":codeExploitation", $codeExploitation);
+                    $stmt->execute();
+                    $idCahierCulture = $stmt->fetch()["idCahierCulture"];
+
+                    foreach ($cahierCulture->parcelles as $parcelle) {
+                        $req = "INSERT INTO parcelle (codeEspece, idCahierCulture, surface, rendementPrevu, rendementRealise) VALUES (:codeEspece, :idCahierCulture, :surface, :rendementPrevu, :rendementRealise)";
+                        $stmt = $cnx->prepare($req);
+                        $stmt->bindParam(":codeEspece", $parcelle->codeEspece);
+                        $stmt->bindParam(":idCahierCulture", $idCahierCulture);
+                        $stmt->bindParam(":surface", $parcelle->surface);
+                        $stmt->bindParam(":rendementPrevu", $parcelle->rendementPrevu);
+                        $stmt->bindParam(":rendementRealise", $parcelle->rendementRealise);
+                        $stmt->execute();
+                    }
                 }
             }
             print("enreg%");
-        }catch(PDOException $e) {
-            print "Erreur :%".$e->getMessage();
+        } catch (PDOException $e) {
+            print "Erreur :%" . $e->getMessage();
             die();
         }
     }
